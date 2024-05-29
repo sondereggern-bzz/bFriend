c11285d599598e4c275422439a46b406aeb2ef69
Samuel Spink <72315013+x47base@users.noreply.github.com>
2024-05-29 10:17:50 +0200

    UPDATE: login route, logout route; ADDED: verifyAdmin function.

--- Backend/authentifizierung.js
+++ Backend/authentifizierung.js
@@ -1,5 +1,5 @@
 /*
-  AUTHOR:              Robin Trachsel
+  VERSION:              Robin Trachsel
   DATE:                 28.05.2024
   DESCRIPTION:          JS-Server: Endpoints for /authentification
 
@@ -14,47 +14,86 @@
     GET: Verify if user is logged in
 */
 
-const express = require('express')
+const express = require('express');
+const crypto = require("node:crypto");
 
-const router = express.Router()
+const router = express.Router();
 
-function verifyAuth(req, res, next) {
-    if (req.session.authenticated) {
-        next()
-    } else {
-        res.status(401).json({ error: 'Unauthorized' })
+const users = [
+    {
+        email: '',
+        password: 'm295'
     }
-}
+];
 
-function queryUser(email, password) {
+const verifyAuth = (req, res, next) => {
+    if (req.session.authenticated) {
+        next();
+    } else {
+        res.status(401).json({ error: 'Unauthorized' });
+    }
+};
+
+const queryUser = (email, password) => {
     // query user from database
     return true
 }
 
-router.post('/login', async (req, res) => {
-    const { email } = req.body
-    const { password } = req.body
-    const user = await express.queryUser(email, password)
-
-    if (user) {
-        req.session.authenticated = true
-        req.session.userId = email // assuming email is unique
-        res.status(201).json({ email })
+const verifyAdmin = (req, res, next) => {
+    if (req.session.authenticated && req.session.userRole && req.session.userRole == "Admin") {
+        next();
     } else {
-        res.status(401).json({ error: 'Login failed' })
+        return res.sendStatus();
     }
-})
+};
+
+const sha256 = (key) => {
+    return crypto.createHash('sha256').update(key).digest('hex');
+};
+
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    
+    if (!username || !password) return res.sendStatus(422);
+    
+    password = sha256(password);
+
+    /* TODO: Switch this to use mysql connection package. */
+    const user = await queryUser(email, password);
+
+    if (!user) return res.status(401).send({ message: "Login has failed. Email or password is invalid." });
+
+    /* TODO: Fetch UserRole from Database */
+    const userRole = "User"; /* Roles: User, Admin */
+
+    let tries = 0;
+    while (!req.session.authenticated && !req.session.userId && tries < 3) {
+        req.session.authenticated = true;
+        req.session.userId = email;         /* email is assumed to be unique. */
+        req.session.userRole = userRole;
+        req.session.save();
+        tries++;
+    }
+
+    return res.status(200).json({ email });
+});
 
 router.delete('/logout', (req, res) => {
-    req.session.authenticated = false
-    res.status(204).end()
-})
+    req.session.destroy(error => {
+        if (error) {
+            return res.sendStatus(500);
+        }
+
+        return res.sendStatus(204);
+    });
+});
 
 router.get('/verify', verifyAuth, (req, res) => {
-    res.status(200).json({ email: req.session.userId })
-})
+    res.status(200).json({ email: req.session.userId });
+});
 
 module.exports = [
     router,
-    verifyAuth
-]
+    verifyAuth,
+    verifyAdmin
+];
