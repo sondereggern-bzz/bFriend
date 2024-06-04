9d3cc1095461931f66b319ad00033449b66121d2
Samuel Spink <dr.greenjet@gmail.com>
2024-06-04 14:19:38 +0200

    ADDED: login page integreated with API

--- Frontend/JS/login.js
+++ Frontend/JS/login.js
@@ -1,18 +1,43 @@
 const BASE_URL = "http://localhost:3000";
 
 /* TODO: Finish this function */
-const login = async (email, password) => {
-    await fetch(`${BASE_URL}/api/authentification/login`,{
+const login = (email, password) => {
+    fetch(`${BASE_URL}/api/authentification/login`,{
         method: "POST",
         body: {
             email: email,
             password: password
         }
     }).then(response => {
-        
+        if (response.ok) {
+            return response.json(); // TODO: Handle whatever is sent back by API
+        } else if (response.error) {
+            console.warn("An error occurd while logging in!");
+            return null;
+        } else {
+            return response.json(); // TODO: Handle whatever is sent back by API
+        }
     }).catch(error => {
         console.warn(`An error occurd while logging in!\n\n${error}`);
+        return null;
     })
 };
 
 /* TODO: Add Event Listener and handle the login */
+document.getElementById("loginForm").addEventListener("submit", (event) => {
+    event.preventDefault();
+    
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const message = document.getElementById("message");
+    
+    if (login(email, password)) {
+        message.style.color = "green";
+        message.textContent = "Success!";
+        /* TODO: Redirect */
+    } else {
+        message.style.color = "red";
+        message.textContent = "Invalid username or password.";
+    }
+});
+
